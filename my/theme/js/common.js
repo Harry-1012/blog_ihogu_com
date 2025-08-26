d869b3144669f90fd7cd6a2bf748c223d3630bb4
harry1012 <harry1012@foxmail.com>
2025-08-26 16:12:52 +0800

    New commit, clear history

---
+++ my/theme/js/common.js
@@ -0,0 +1,2 @@
+document.querySelector('#aside-content #card-info-btn').addEventListener('click',function(){let queryArr=location.href.split('/')
+location.href=queryArr[0]+'//'+queryArr[2]+'/'+queryArr[3]+'/'+queryArr[4]+'/contact'})
